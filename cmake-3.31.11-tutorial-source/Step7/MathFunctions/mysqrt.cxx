78c77be1f6cf4dfb8a714d80cd34899f81d451a3
abin <abin.z@outlook.com>
2026-02-04 00:17:52 +0800

    feat: 实现Step7案例

--- cmake-3.31.11-tutorial-source/Step7/MathFunctions/mysqrt.cxx
+++ cmake-3.31.11-tutorial-source/Step7/MathFunctions/mysqrt.cxx
@@ -1,13 +1,13 @@
 #include "mysqrt.h"
 
 // TODO 4: include cmath
+#include <cmath>
 #include <iostream>
 
 namespace mathfunctions {
 namespace detail {
 // a hack square root calculation using simple operations
-double mysqrt(double x)
-{
+double mysqrt(double x) {
   if (x <= 0) {
     return 0;
   }
@@ -20,6 +20,14 @@ double mysqrt(double x)
 
   // Hint: Don't forget the #endif before returning the result!
 
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+
+  double result = std::exp(std::log(x) * 0.5);
+  std::cout << "Computing sqrt of " << x << " to be " << result
+            << " using log and exp" << std::endl;
+
+#else
+
   double result = x;
 
   // do ten iterations
@@ -31,8 +39,8 @@ double mysqrt(double x)
     result = result + 0.5 * delta / result;
     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
   }
-
+#endif
   return result;
 }
-}
-}
+}  // namespace detail
+}  // namespace mathfunctions
